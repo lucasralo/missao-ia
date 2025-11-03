f795fdd50fb1fe25d344cb629994686212933e3b
lucasralo <162320950+lucasralo@users.noreply.github.com>
2025-11-03 09:47:40 -0300

    03112025

---
+++ java/perguntas.js
@@ -0,0 +1,12 @@
+export const perguntas = [
+    }
+    enunciado do problema:"A forma como uma sociedade lida com o porte de armas hoje moldará o ambiente de segurança e os desafios de violência que a próxima geração (os jovens do futuro) irá herdar. A legislação atual sobre o acesso a armas de fogo, o treinamento e a educação cívica sobre o tema, e a própria cultura de segurança, serão os alicerces da vida adulta desses jovens."
+alternativas:[
+    {
+        texto: "A forma como uma sociedade lida com o porte de armas hoje moldará o ambiente de segurança e os desafios de violência que a próxima geração (os jovens do futuro) irá herdar. A legislação atual sobre o acesso a armas de fogo, o treinamento e a educação cívica sobre o tema, e a própria cultura de segurança, serão os alicerces da vida adulta desses jovens.";
+        afirmacao: [
+            "A flexibilização do porte de armas para cidadãos com ficha limpa e treinamento adequado resulta em uma redução imediata na criminalidade violenta, devido ao maior potencial de dissuasão.";
+        ]
+    }
+]
+]
